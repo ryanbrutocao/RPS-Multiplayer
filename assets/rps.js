3e83bbd2d01c2515564970f1e9cccc080bfb85cb
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-07-30 18:40:59 -0600

    completed functional parts of the rock paper scissors game using firebase

--- assets/rps.js
+++ assets/rps.js
@@ -24,4 +24,124 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+
+// on button click, log user choice to firebase... if both user one and user two have chosen, run the checking function
+
+$("#playerOne").on("click", ".p1Button", function() {
+  // alert("working")
+  event.preventDefault();
+  var player1Choice = $(this).attr("value");
+  $(this).attr("data-val", "true");
+  $("#playerOne").attr("data-live", "true");
+  
+  var playerData = {
+    player1: player1Choice
+    }
+  database.ref().update(playerData);
+  
+  // console.log(player1Choice);
+}); 
+
+$("#playerTwo").on("click", ".p2Button", function() {
+  // alert("working")
+  event.preventDefault();
+  var player2Choice = $(this).attr("value");
+  $(this).attr("data-val", "true");
+  $("#playerTwo").attr("data-live", "true");
+  
+  var playerData = {
+    player2: player2Choice
+ 
+  }
+  database.ref().update(playerData);
+  
+  // console.log(player2Choice);
+});
+
+
+database.ref().on("value", function (childSnapshot) {
+  var p1Choice = childSnapshot.val().player1;
+  // $("#playerOne").attr("data-live", "true");
+  var p2Choice = childSnapshot.val().player2;
+  // $("#playerTwo").attr("data-live", "true");
+
+  console.log(p1Choice);
+  console.log(p2Choice);
+
+  // if ($("#playerOne").val("data-live", "true") &&  $("#playerTwo").val("data-live", "true")){
+  //   alert("working!")
+  // }
+
+  // if ((p1Choice === "Rock") || (p1Choice === "Paper") || (p1Choice === "Scissors")) {}
+  
+   if (
+      (p1Choice === "Rock" && p2Choice === "Scissors") ||
+      (p1Choice === "Scissors" && p2Choice === "Paper") || 
+      (p1Choice === "Paper" && p2Choice === "Rock")
+      ) 
+      {
+      p1Wins++;
+      p2Losses++;
+      $("#display").text("Player 1 WINS!!!")
+      clear();
+      } 
+     else if (
+      (p2Choice === "Rock" && p1Choice === "Scissors") ||
+      (p2Choice === "Scissors" && p1Choice === "Paper") || 
+      (p2Choice === "Paper" && p1Choice === "Rock")){
+      p2Wins++;
+      p1Losses++;
+      $("#display").text("Player 2 WINS!!!")
+      clear();
+    } else if (p1Choice === p2Choice) {
+      ties++;
+      $("#display").text("Tie Game, Play Again.")
+      clear();
+    }
+
+    // console.log(p1Wins);
+    $("#p1Wins").text(p1Wins)
+    // console.log("player 1 wins: ", p1Wins);
+    $("#p1Losses").text(p1Losses);
+    // console.log("player 1 losses: ", p1Losses);
+    $("#ties").text(ties)
+    // console.log("ties: ", ties);
+    $("#p2Wins").text(p2Wins)
+    // console.log("player 2 wins: ", p2Wins);
+    $("#p2Losses").text(p2Losses);
+    // console.log("player 2 losses: ", p2Losses);
+  }
+  )
+  
+  function clear () {
+
+    database.ref().remove()
+  }
+
+
+
+
+
+
+  //  // function that allows the user to click a still gif and turn it on or off
+  //  $("#displayGifs").on("click", ".gif", function() {
+  //   var state = $(this).attr("data-state");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
